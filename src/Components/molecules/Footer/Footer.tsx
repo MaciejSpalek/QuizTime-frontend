28de59a81a80168d8d717229b42dfd3d5dda1009
MaciejSpalek <spwrtt@gmail.com>
2021-03-14 21:23:58 +0100

    Add linkedin icon to footer

--- src/Components/molecules/Footer/Footer.tsx
+++ src/Components/molecules/Footer/Footer.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { 
-    StyledParagraph,
-    StyledWrapper, 
-    StyledFooter, 
-    StyledAnchor,
-    StyledIcon 
-} from './Footer.styled';
+import React from "react";
+import {
+  StyledParagraph,
+  StyledWrapper,
+  StyledFooter,
+  StyledAnchor,
+  StyledIcon,
+} from "./Footer.styled";
 
 const Footer = () => {
-    return (
-        <StyledFooter data-testid="Footer">
-            <StyledWrapper>
-                <StyledParagraph text='&copy; 2021 - Maciej Spałek' />
-            </StyledWrapper>
-            <StyledWrapper>
-                <StyledAnchor aria-label="Check github profile" href="https://github.com/MaciejSpalek/QuizTime-frontend" >
-                    <StyledIcon icon={['fab', 'github-square']}/>
-                </StyledAnchor>
-            </StyledWrapper>
-        </StyledFooter>
-    );
+  return (
+    <StyledFooter data-testid="Footer">
+      <StyledWrapper>
+        <StyledParagraph text="&copy; 2021 - Maciej Spałek" />
+      </StyledWrapper>
+      <StyledWrapper>
+        <StyledAnchor
+          aria-label="Check github profile"
+          href="https://github.com/MaciejSpalek/QuizTime-frontend"
+        >
+          <StyledIcon icon={["fab", "github-square"]} />
+        </StyledAnchor>
+        <StyledAnchor
+          aria-label="Check github profile"
+          href="https://www.linkedin.com/in/maciej-spałek"
+        >
+          <StyledIcon icon={["fab","linkedin"]} />
+        </StyledAnchor>
+      </StyledWrapper>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
